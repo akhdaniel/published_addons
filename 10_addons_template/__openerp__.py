5d0e7283919919d4dceb3544e02ae1c8130c4946
akhdaniel <akhmad.daniel@gmail.com>
2016-11-17 13:19:39 +0700

    Add files via upload

---
+++ 10_addons_template/__openerp__.py
@@ -0,0 +1,30 @@
+{
+	"name": "Information",
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"board"
+	],
+	"author": "akhmad.daniel@gmail.com", 
+	"category": "Education", 
+	'website': 'http://www.vitraining.com',
+	"description": """\
+
+Manage
+======================================================================
+
+* this is my academic information system module
+* created menu:
+* created object
+* created views
+* logic:
+
+""",
+	"data": [
+		"security/groups.xml",
+		"menu.xml", 
+	],
+	"installable": True,
+	"auto_install": False,
+    "application": True,
+}
