6e57363135bfca7ebcc66e5ea6f7084382f3271d
akhdaniel <akhmad.daniel@gmail.com>
2016-11-17 13:21:05 +0700

    Add files via upload

---
+++ 08_addons_template/__openerp__.py
@@ -0,0 +1,27 @@
+{
+	"name": "Information", 
+	"version": "1.0", 
+	"depends": [
+		"base",
+		"board"
+	], 
+	"author": "akhmad.daniel@gmail.com", 
+	"category": "Education", 
+	"description": """\
+
+Manage
+======================================================================
+
+* this is my academic information system module
+* created menu:
+* created object
+* created views
+* logic:
+
+""",
+	"data": [
+		"menu.xml", 
+	],
+	"installable": True,
+	"auto_install": False,
+}
